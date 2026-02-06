d0666e988e5b8faac1c6619a238c495c593b3ff1
the-423 <kaseycarrara@gmail.com>
2026-02-05 23:53:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quotes/',,  // Required for GitHub Pages
+  base: '/quotes/',  // Required for GitHub Pages
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
